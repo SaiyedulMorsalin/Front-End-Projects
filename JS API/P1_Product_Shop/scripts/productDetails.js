6cef8a39cf51b3912228ae1b136d4e2af31e9c71
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-08-23 11:44:19 +0600

    added product details

--- JS API/P1_Product_Shop/scripts/productDetails.js
+++ JS API/P1_Product_Shop/scripts/productDetails.js
@@ -3,6 +3,9 @@ const productId = new URLSearchParams(window.location.search).get("product_id");
 const singleProduct = async (productId) => {
     const res = await fetch(`https://fakestoreapi.com/products/${productId}`);
     const product = await res.json();
+    const productDetailContainer = document.getElementById("productDetail");
+    const productDiv = document.createElement("div")
+
 
 }
 
